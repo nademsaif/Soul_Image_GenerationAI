43325e64bbf572024f702a7da4dd3f0f0c5c6d67
Nadeem Saif <73422139+naee44@users.noreply.github.com>
2023-03-06 17:44:43 +0200

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/soul', {
+        const response = await fetch('https://soul-image-generation.onrender.com/api/v1/soul', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://soul-image-generation.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
